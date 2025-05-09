81b14a6d22590492ac590e50aefba1eef31f3382
atinseau <arthurtinseau@live.fr>
2025-05-10 01:27:31 +0200

    syncro

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,10 +1,7 @@
 import { LinkPopup } from "@/components/LinkPopup";
 import { getFacebookAuthUrl } from "@/lib/functions/getFacebookAuthUrl.server";
 
-
-
 export default async function Home() {
-
   return (<div>
     <LinkPopup url={getFacebookAuthUrl()}>Facebook oauth</LinkPopup>
   </div>);
---
+++ apps/client/src/lib/db/prisma.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "@smart-moderation-ai/db"
+
+export const prisma = new PrismaClient()
