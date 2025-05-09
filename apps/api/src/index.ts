1b9a5dfa498d273633e7fc5937e0ff059d902259
atinseau <arthurtinseau@live.fr>
2025-05-10 01:24:22 +0200

    syncro

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 import { cors } from '@elysiajs/cors'
-
 import { facebookPlugin } from "./plugins/facebook";
+import { prisma } from "./lib/prisma";
+
 
 const app = new Elysia()
   .use(cors())
